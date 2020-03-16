17ca0a428884e9339eb4ad3e79d4d5d8b597b206
SidBharthulwar <siddharth.bharthulwar@gmail.com>
2020-03-16 13:35:09 -0600

    buggy exponential

--- region.py
+++ region.py
@@ -1,7 +1,8 @@
 import numpy as np 
 import matplotlib.pyplot as plt 
 import sklearn
-
+from Regression.exponential import exponential
+from scipy import optimize
 
 class Region:  
     
@@ -16,28 +17,31 @@ class Region:
         self.rowData = rowData
 
         self.sets = []
-        numList = range(0, len(self.rowData))
+        self.numList = range(0, len(self.rowData))
         for i in range(len(self.rowData)):
 
-            self.sets.append((numList[i], self.rowData[i]))
+            self.sets.append((self.numList[i], self.rowData[i]))
 
         #MODELS:
-
+        self.exponentialModel()
+    def quadraticModel(self):
         quadraticModel = np.polyfit(numList, self.rowData, 2)
-        polynomialmodel = np.polyfit(numList, self.rowData, 10)
 
         quad1dim = np.poly1d(quadraticModel)
-        poly1dim = np.poly1d(polynomialmodel)
         plt.scatter(numList, self.rowData)
         lin = np.linspace(0, len(self.rowData))
         plt.plot(lin, quad1dim(lin))
         plt.title("Quad for: " + self.countryName)
         plt.show()
-        #END MODELS
-        '''
-        plt.scatter(numList, self.rowData)
-#        plt.plot(numList, self.coefficient * numList + self.intercept)
-        plt.title("LogReg for: " + self.countryName)
+    def exponentialModel(self):
+        popt_exponential, pcov_exponential = optimize.curve_fit(exponential, self.numList, self.rowData)
+
+        lins = np.linspace(0, len(self.rowData), 100)
+        vals = exponential(lins, popt_exponential[0], popt_exponential[1], popt_exponential[2])
+
+        plt.scatter(self.numList, self.rowData)
+        plt.plot(lins, vals)
+        plt.title("Exp for: " + self.countryName)
         plt.show()
-        '''
+
 
