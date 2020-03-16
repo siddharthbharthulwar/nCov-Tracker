44c6896e16838bf4b14a8984c3b530c72a807211
SidBharthulwar <siddharth.bharthulwar@gmail.com>
2020-03-16 15:49:47 -0600

    successful exponential regression

--- region.py
+++ region.py
@@ -1,6 +1,7 @@
 import numpy as np 
 import matplotlib.pyplot as plt 
 import sklearn
+import math
 from Regression.exponential import exponential
 from scipy import optimize
 
@@ -10,8 +11,10 @@ class Region:
     def __init__(self, countryName, regionName, rowData):  
 
         self.countryName = countryName
-        if not regionName == 'nan':
+        if type(regionName) == str:
             self.regionName = regionName
+        else:
+            self.regionName = " "
 
         self.rawRowData = rowData    
         self.rowData = rowData
@@ -23,7 +26,9 @@ class Region:
             self.sets.append((self.numList[i], self.rowData[i]))
 
         #MODELS:
-        self.exponentialModel()
+        if (self.rowData[len(self.rowData) - 1] > 500):
+
+            self.exponentialModel()
     def quadraticModel(self):
         quadraticModel = np.polyfit(numList, self.rowData, 2)
 
@@ -34,14 +39,21 @@ class Region:
         plt.title("Quad for: " + self.countryName)
         plt.show()
     def exponentialModel(self):
-        popt_exponential, pcov_exponential = optimize.curve_fit(exponential, self.numList, self.rowData)
+        popt_exponential, pcov_exponential = optimize.curve_fit(exponential, self.numList, 
+        self.rowData, bounds = ((1e-05, 0, -15), (1, 5e-01, 15)))
 
         lins = np.linspace(0, len(self.rowData), 100)
         vals = exponential(lins, popt_exponential[0], popt_exponential[1], popt_exponential[2])
 
         plt.scatter(self.numList, self.rowData)
         plt.plot(lins, vals)
-        plt.title("Exp for: " + self.countryName)
+        if (self.regionName == " "):
+            plt.title("Exp for: " + self.countryName)
+
+        else:
+            plt.title("Exp for: " + self.regionName + ", " + self.countryName)
+
         plt.show()
+        print(popt_exponential)
 
 
