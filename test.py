a9e2648c1241791aa65a306fe011f992e398f21a
Siddharth Bharthulwar <47193327+siddharthbharthulwar@users.noreply.github.com>
2020-04-02 11:45:51 -0600

    Delete test.py

--- test.py
+++
@@ -1,6 +0,0 @@
-import numpy as np 
-
-predictions = [1, 7, 4, 345 ,7, 2334, 7, 86, 765, 23, 36 ,675, 6435 ,325 ,34, 356 ,345, 3543, 5435 ,435,43, 53, ]
-x = sorted(range(len(predictions)), key=lambda k: predictions[k])
-x.reverse()
-print(x[: 3])
